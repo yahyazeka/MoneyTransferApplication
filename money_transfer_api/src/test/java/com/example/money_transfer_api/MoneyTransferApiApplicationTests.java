e27a6d17f4f62e8b7c25fd8f982882309b306ae2
yahyazeka <36335592+yahyazeka@users.noreply.github.com>
2021-04-28 16:54:56 +0300

    inital commit

---
+++ money_transfer_api/src/test/java/com/example/money_transfer_api/MoneyTransferApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.money_transfer_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyTransferApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
